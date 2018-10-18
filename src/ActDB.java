2d823e2b24a238fd46d3f8d8aa5b8a25e536f6c1
warog <ehnrike-shvelidze@yandex.ru>
2018-10-18 11:37:36 +0300

    add a delMethods

--- src/ActDB.java
+++ src/ActDB.java
@@ -1,5 +1,6 @@
 import org.sqlite.JDBC;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 
@@ -22,7 +23,7 @@ public class ActDB {
         this.connection = DriverManager.getConnection(bdPath);
     }
 
-    public List<Project> getAllProject() {
+    public List<Project> getAllProjects() {
 
         try(Statement statement = this.connection.createStatement()) {
             List<Project> projects = new ArrayList<Project>();
@@ -83,6 +84,30 @@ public class ActDB {
 
     }
 
+    public void getIssue(String projectName, String userName){
+
+        try(Statement statement = this.connection.createStatement()) {
+
+            ResultSet getUID_PID = statement.executeQuery("SELECT u.id AS \"User ID\", p.id AS \"Project ID\" FROM Users u, Projects p\n" +
+                    "WHERE p.name = \"" + projectName + "\" AND u.name = \"" + userName + "\"");
+
+            int userId = getUID_PID.getInt("User ID");
+            int projectId = getUID_PID.getInt("Project ID");
+
+            ResultSet resultSet = statement.executeQuery("SELECT u.name AS \"User Name\", p.name AS \"Project Name\", i.Issue FROM Users u, Projects p, Issues i\n" +
+                    "WHERE p.id = " + projectId + " AND u.id = " + userId + " AND u.id = i.User_id\n" +
+                    "GROUP BY i.Issue");
+
+            System.out.println("Project Name: " + projectName + " User Name: " + userName + " User Issue: ");
+            while(resultSet.next()){
+                System.out.println( resultSet.getString(3));
+            }
+
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void addProject(Project project){
 
         try(PreparedStatement statement = this.connection.prepareStatement("INSERT INTO Projects('Name')" + "VALUES(?)")){
@@ -118,27 +143,46 @@ public class ActDB {
         }
     }
 
-    public void getIssue(String projectName, String userName){
+    public void deleteProject(int id){
 
-        try(Statement statement = this.connection.createStatement()) {
+        try(PreparedStatement statement = this.connection.prepareStatement("DELETE FROM Projects WHERE id = ?")){
 
-            ResultSet getUID_PID = statement.executeQuery("SELECT u.id AS \"User ID\", p.id AS \"Project ID\" FROM Users u, Projects p\n" +
-                    "WHERE p.name = \"" + projectName + "\" AND u.name = \"" + userName + "\"");
+            statement.setInt(1, id);
+            statement.execute();
 
-            int userId = getUID_PID.getInt("User ID");
-            int projectId = getUID_PID.getInt("Project ID");
-
-            ResultSet resultSet = statement.executeQuery("SELECT u.name AS \"User Name\", p.name AS \"Project Name\", i.Issue FROM Users u, Projects p, Issues i\n" +
-                                                                "WHERE p.id = " + projectId + " AND u.id = " + userId + " AND u.id = i.User_id\n" +
-                                                                "GROUP BY i.Issue");
-
-            System.out.println("Project Name: " + projectName + " User Name: " + userName + " User Issue: ");
-            while(resultSet.next()){
-                System.out.println( resultSet.getString(3));
-            }
-
-        }catch(SQLException e) {
+        }catch (SQLException e){
             e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int id){
+
+        try(PreparedStatement statement = this.connection.prepareStatement("DELETE FROM PUsers WHERE id = ?")){
+
+            statement.setInt(1, id);
+            statement.execute();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteIssue(int id){
+
+        try(PreparedStatement statement = this.connection.prepareStatement("DELETE FROM Issues WHERE id = ?")){
+
+            statement.setInt(1, id);
+            statement.execute();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readList(List list){
+        List projects = list;
+        for (Object project : projects) {
+            System.out.println(project.toString());
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
                     "\naddProject ProjectName" +
                     "\naddIssue UserID IssueName");
 
-            System.out.println("\nВведите одну из команд - (addUser | addProject | addIssue | getProjects | getUsers | getIssues | exit): ");
+            System.out.println("\nВведите одну из команд - (addUser | addProject | addIssue | getProjects | getUsers | getIssues | delProject |delUser | delIssue | exit): ");
 
             String[] params = bufferedReader.readLine().trim().split(" ");
             switch (params[0]){
@@ -26,63 +26,43 @@ public class Main {
                     break;
                 case "getUsers":
                     System.out.println("\t \u001B[33m -Users- \u001B[0m");
-                    List<User> users = actDB.getAllUsers();
-                    for (User user : users) {
-                        System.out.println(user.toString());
-                    }
+                    actDB.readList(actDB.getAllUsers());
                     break;
                 case "addProject":
                     actDB.addProject(new Project(0, params[1]));
                     break;
                 case "getProjects":
                     System.out.println("\t \u001B[33m -Projects- \u001B[0m");
-                    List<Project> projects = actDB.getAllProject();
-                    for (Project project : projects) {
-                        System.out.println(project.toString());
-                    }
+                    actDB.readList(actDB.getAllProjects());
                     break;
                 case "addIssue":
                     actDB.addIssue(new Issue(0, Integer.valueOf(params[1]), params[2]));
                     break;
                 case "getIssues":
                     System.out.println("\t \u001B[33m -Issues- \u001B[0m");
-                    List<Issue> issues = actDB.getAllIssues();
-                    for (Issue issue : issues) {
-                        System.out.println(issue.toString());
-                    }
+                    actDB.readList(actDB.getAllIssues());
                     break;
                 case "getIssue":
                     actDB.getIssue(params[1], params[2]);
+                    break;
+                case "delProject":
+                    actDB.deleteProject(Integer.valueOf(params[1]));
+                    actDB.readList(actDB.getAllProjects());
+                    break;
+                case "delUser":
+                    actDB.deleteUser(Integer.valueOf(params[1]));
+                    actDB.readList(actDB.getAllUsers());
+                    break;
+                case "delIssue":
+                    actDB.deleteIssue(Integer.valueOf(params[1]));
+                    actDB.readList(actDB.getAllIssues());
                     break;
                 case "exit":
                     System.out.println("Good Bye");
                     return;
                 default:
-                    System.out.println("Неверная команда");
+                    System.out.println("invalid command");
             }
         }
-//        try {
-//            actDB.addProject(new Project(0, "SPAM"));
-//            actDB.addUser(new User(0, 1, "Ulia"));
-//            actDB.addIssue(new Issue(0, 2, "Make a space pen"));
-//
-//            List<Project> projects= actDB.getAllProject();
-//            for (Project project : projects) {
-//                System.out.println(project.toString());
-//            }
-//
-//            List<User> users = actDB.getAllUsers();
-//            for (User user : users) {
-//                System.out.println(user.toString());
-//            }
-//
-//            List<Issue> issues = actDB.getAllIssues();
-//            for (Issue issue : issues) {
-//                System.out.println(issue.toString());
-//            }
-//
-//        }catch(SQLException e){
-//            e.printStackTrace();
-//        }
     }
 }
